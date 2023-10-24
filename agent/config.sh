3806bedcf2a453120ef13c12b6329c14b913f6ad
bottlenome <bottlenome@gmail.com>
2023-10-24 21:05:42 +0900

    Enable to get issue list.

---
+++ agent/config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+GITHUB_OWNNER="bottlenome"
+GITHUB_REPO="NeuralNetNest"
+WANDB_SCORE_NAME="cv"
+SCORE_DIRECTION="minimize"
+EXP_CODE_PATH="../run.py"
+LLM_MODEL="gpt-4"
---
+++ agent/fetch_github_issues.py
@@ -0,0 +1,22 @@
+import os
+import requests
+import pandas as pd
+
+
+def fetch_github_issues(owner, repo):
+    """
+    githubのissuesの一覧を取得。pandas dataframeとして返す
+    """
+    token = os.getenv("GITHUB_TOKEN")
+    if token is None:
+        print("Make sure to set GUTHUB_TOKEN")
+        exit()
+
+    url = f"https://api.github.com/repos/{owner}/{repo}/issues?state=open"
+    headers = {
+        "Accept": "application/vnd.github.v3+json", 
+        "Authorization": "token " + token
+    }
+    
+    issues = requests.get(url, headers=headers).json()
+    return pd.DataFrame(issues)
